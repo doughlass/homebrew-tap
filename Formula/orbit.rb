4a59235918058c80a9a73032d572bb456880ae09
doughlass <hello@madebysloths.co>
2026-08-24 09:20:14 +0100

    orbit v1.1.0

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -1,26 +1,26 @@
 class Orbit < Formula
   desc "Terminal UI for AWS"
   homepage "https://github.com/doughlass/orbit"
-  version "1.0.1"
+  version "1.1.0"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/doughlass/orbit/releases/download/v1.0.1/orbit-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "3eabefe1d6b6628ae7245069280cfec4a8f09889d6c6bcf2353bf4e93c796b76"
+      url "https://github.com/doughlass/orbit/releases/download/v1.1.0/orbit-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "8043d07f3f8612e078e007d080ff89e675a79408ad3eeb3851fe8372445e8521"
     else
-      url "https://github.com/doughlass/orbit/releases/download/v1.0.1/orbit-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "9cd49cf9a18b2be2e7e51e0fe07cd9844d5d0aa1d8e300aedb71477acf56521d"
+      url "https://github.com/doughlass/orbit/releases/download/v1.1.0/orbit-tui-x86_64-apple-darwin.tar.gz"
+      sha256 "5936ed0ffcde6bfc3f09813422927a8264c05e126844ed0ebf3bf9785ec884e8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/doughlass/orbit/releases/download/v1.0.1/orbit-tui-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "6a8656fc6059a61038f0812e82587f36437be3ac7178d5609c161a41b1a6228d"
+      url "https://github.com/doughlass/orbit/releases/download/v1.1.0/orbit-tui-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "df2d4ef74ce391538ac2261113c400b9e1a6022b659df415f2670acddecc26c1"
     else
-      url "https://github.com/doughlass/orbit/releases/download/v1.0.1/orbit-tui-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "af2de18f1b7bdc53a0c3b25cdf7223829e4508c128a6c8cc69c3e9448e3cfbac"
+      url "https://github.com/doughlass/orbit/releases/download/v1.1.0/orbit-tui-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "b45ec50a5d0b3512b169e370a0a42329655a8739c64857ac5465e644cde627d4"
     end
   end
 
